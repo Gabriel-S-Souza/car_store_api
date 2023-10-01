0fd13324e2cffcc374079951192e5ab5e09a351a
Gabriel Souza <gabriel12saraiva@gmail.com>
2023-10-01 16:55:54 -0300

    feat: sets maximun limit of the payload size

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,22 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { createSwaggerDocument } from './helpers/documentation.helper';
 import { customValidationPipe } from './helpers/commom.helper';
+import * as express from 'express';
+import { ExpressAdapter } from '@nestjs/platform-express';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+const server: express.Express = express();
+server.use(express.json({ limit: '50mb' }));
+server.use(express.urlencoded({ extended: true, limit: '50mb' }));
+
+async function bootstrap(expressInstance: express.Express) {
+  const app = await NestFactory.create(
+    AppModule,
+    new ExpressAdapter(expressInstance),
+  );
+  app.enableCors();
   app.setGlobalPrefix('/api');
   app.useGlobalPipes(customValidationPipe);
   createSwaggerDocument(app);
   await app.listen(3000);
 }
-bootstrap();
+bootstrap(server);
